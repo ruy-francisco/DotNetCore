ec6d344df0abccd046b1d1c39e974168a7295d4d
ruy-francisco <ruy.fsalmeida@gmail.com>
2018-04-03 16:29:40 -0300

    Get first data from form

---
+++ DoubleRoundView/interaction/userInput.js
@@ -0,0 +1,12 @@
+var NewDoubleRoundForm = new Vue({
+	el: '#NewDoubleRoundForm',
+	data: {
+		placeName: '',
+		placeAddress: ''
+	},
+	methods: {
+		insertDoubleRound: function() {
+			alert(placeAddress.value);
+		}
+	}
+});
